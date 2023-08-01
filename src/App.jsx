f69e05e1d3f45185441f4997e47890c872f845de
Anil parajuli <anilparajuli580@gmail.com>
2023-07-31 19:31:17 -0700

    compliting home component with its funcationality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Component/Header/Header";
+import Home from "./Page/Home/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Header />
         <Routes>
+          <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1>movie detail page</h1>}></Route>
           <Route path="movie/:type" element={<h1>movie list page</h1>}></Route>
           <Route path="/*" element={<h1>Error page</h1>}></Route>
